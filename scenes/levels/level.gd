2660ef4eef867c319514ed9822de41435531980a
Legacynical <legacynical@gmail.com>
2024-06-23 22:01:07 -0700

    remove Logo code in level.gd

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -2,24 +2,6 @@ extends Node2D
 
 var test_array: Array[String] = ["Test", "fizz", "buzz"]
 
-func _ready():
-	#get_node("Logo").rotation_degree = 90
-	$Logo.rotation_degrees = 90 # same as above
-	
-	for i in test_array:
-		print(i)
-	
-	
-func _process(delta):
-	$Logo.rotation_degrees += 100 * delta
-	
-	# flow -> if rotation > 100 -> 0
-	#if $Logo.rotation_degrees > 180:
-	#	$Logo.rotation_degrees = 0
-	
-	if $Logo.position.x > 1000:
-		$Logo.pos.x = 0
-	#print(Input.is_action_pressed("left"))
 
 func test_function():
 	print("This is a test function")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,14 +2,14 @@ extends CharacterBody2D
 
 var speed = 500
 
-func _process(delta):
+func _process(_delta):
 	
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
 	#print(direction)
 	#position += direction * speed * delta (not used for CharacterBody2D)
 	velocity = direction * speed
-	move_and_slide()
+	move_and_slide() # includes delta
 	
 	# laser shooting input
 	if Input.is_action_pressed("primary action"):
