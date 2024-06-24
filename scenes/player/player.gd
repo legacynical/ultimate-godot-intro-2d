2875e88dc6c1367d35d6113cf466250ce84946f0
Legacynical <legacynical@gmail.com>
2024-06-23 20:42:53 -0700

    add basic input maps

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,7 +1,17 @@
 extends Node2D
 
+var speed = 500
+
 func _process(delta):
 	
 	# input
 	var direction = Input.get_vector("left", "right", "up", "down")
-	print(direction)
+	#print(direction)
+	position += direction * speed * delta
+
+	# laser shooting input
+	if Input.is_action_pressed("primary action"):
+		print("shoot laser")
+	
+	if Input.is_action_pressed("secondary action"):
+		print("secondary action")
