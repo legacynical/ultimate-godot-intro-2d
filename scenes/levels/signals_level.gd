1c1a49e9b80b54cbcd95e44b8f9d33a424a67274
Legacynical <legacynical@gmail.com>
2024-06-25 09:02:03 -0700

    add custom signals for player weapons

--- scenes/levels/signals_level.gd
+++ scenes/levels/signals_level.gd
@@ -4,3 +4,11 @@ extends Node2D
 func _on_gate_player_entered_gate(body):
 	print("player entered gate")
 	print(body)
+
+
+func _on_player_laser():
+	print("laser from level")
+
+
+func _on_player_grenade():
+	print("grenade from level")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+signal laser
+signal grenade
+
 var can_laser: bool = true
 var can_grenade: bool = true
 
@@ -16,10 +19,12 @@ func _process(_delta):
 	
 	# laser shooting input
 	if Input.is_action_pressed("primary action") and can_laser:
+		laser.emit()
 		print("shoot laser")
 		can_laser = false
 	
 	if Input.is_action_pressed("secondary action") and can_grenade:
+		grenade.emit()
 		print("shoot grenade")
 		can_grenade = false
 
