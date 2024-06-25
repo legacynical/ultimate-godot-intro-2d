757c451687ca007149364c90a59d130e0e203aee
Legacynical <legacynical@gmail.com>
2024-06-25 11:50:12 -0700

    fix start timer calls

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -21,16 +21,16 @@ func _process(_delta):
 	if Input.is_action_pressed("primary action") and can_laser:
 		can_laser = false
 		laser.emit()
+		$LaserTimer.start()
 	
 	if Input.is_action_pressed("secondary action") and can_grenade:
 		can_grenade = false
 		grenade.emit()
+		$GrenadeTimer.start()
 
 
 func _on_laser_timer_timeout():
 	can_laser = true
-	$LaserTimer.start()
 	
 func _on_grenade_timer_timeout():
 	can_grenade = true
-	$GrenadeTimer.start()
