75b4b789203bfc80505f23290e7f67b5841bc8fd
Legacynical <legacynical@gmail.com>
2024-06-25 06:20:36 -0700

    add signals to gate entry and player weapons

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var can_laser: bool = true
+var can_grenade: bool = true
+
 var speed = 500
 
 func _process(_delta):
@@ -12,8 +15,17 @@ func _process(_delta):
 	move_and_slide() # includes delta
 	
 	# laser shooting input
-	if Input.is_action_pressed("primary action"):
+	if Input.is_action_pressed("primary action") and can_laser:
 		print("shoot laser")
+		can_laser = false
 	
-	if Input.is_action_pressed("secondary action"):
+	if Input.is_action_pressed("secondary action") and can_grenade:
 		print("shoot grenade")
+		can_grenade = false
+
+
+func _on_laser_timer_timeout():
+	can_laser = true
+
+func _on_grenade_timer_timeout():
+	can_grenade = true
