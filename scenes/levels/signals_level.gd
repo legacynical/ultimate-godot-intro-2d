a8a8ce3fcda5dfb971d8f8546e3c96aa8fe4ff6f
Legacynical <legacynical@gmail.com>
2024-06-25 12:02:44 -0700

    add laser node instancing

--- scenes/levels/signals_level.gd
+++ scenes/levels/signals_level.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 
 func _on_gate_player_entered_gate(body):
 	print("player entered gate")
@@ -7,6 +8,8 @@ func _on_gate_player_entered_gate(body):
 
 
 func _on_player_laser():
+	var laser = laser_scene.instantiate()
+	add_child(laser)
 	print("laser from level")
 
 
