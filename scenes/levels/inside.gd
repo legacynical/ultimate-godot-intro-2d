dac58583bacc35968c1be49a2f6644a53380fe14
Legacynical <legacynical@gmail.com>
2024-07-08 14:29:15 -0700

    fix scene transition error messages

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,6 +1,12 @@
 extends LevelParent
 
+@export var outside_level_scene: PackedScene
+
 func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
+	#get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+	call_deferred("change_scene")	
+	
+func change_scene():
 	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -3,4 +3,7 @@ extends LevelParent
 func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
+	call_deferred("change_scene")	
+	
+func change_scene():
 	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
