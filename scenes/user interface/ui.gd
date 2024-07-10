5fea0b6d234268bf1ff27651e560a9ca41fdf246
Legacynical <legacynical@gmail.com>
2024-07-09 23:41:53 -0700

    add weapon ammo ui color change

--- scenes/user interface/ui.gd
+++ scenes/user interface/ui.gd
@@ -4,6 +4,8 @@ extends CanvasLayer
 var green: Color = Color("6bbfa3")
 var red: Color = Color(0.9, 0, 0, 1)
 
+#onready var are basically _ready() declared var that can be used globally
+#outside of the _ready() func
 @onready var laser_label: Label = $LaserCounter/VBoxContainer/Label
 @onready var grenade_label: Label = $GrenadeCounter/VBoxContainer/Label
 @onready var laser_icon: TextureRect = $LaserCounter/VBoxContainer/TextureRect
@@ -16,8 +18,17 @@ func _ready():
 	
 func update_laser_text():
 	laser_label.text = str(Globals.laser_amount)
-	laser_label.modulate = green
+	update_color(Globals.grenade_amount, laser_label, laser_icon)
 	
 func update_grenade_text():
 	grenade_label.text = str(Globals.grenade_amount)
-	grenade_label.modulate = red
+	update_color(Globals.grenade_amount, grenade_label, grenade_icon)
+
+func update_color(amount: int, label: Label, icon: TextureRect) -> void:
+	if amount <= 0:
+		label.modulate = red
+		icon.modulate = red
+	else:
+		label.modulate = green
+		icon.modulate = green
+		
