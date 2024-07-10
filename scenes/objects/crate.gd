aa63a914977692caa7ee2d7dd365d63e13bafd27
Legacynical <legacynical@gmail.com>
2024-07-10 16:30:47 -0700

    fix item spawn position randomness

--- scenes/objects/crate.gd
+++ scenes/objects/crate.gd
@@ -3,6 +3,6 @@ extends ItemContainer
 func hit():
 	$LidSprite.hide()
 	# pick random marker from spawnPositions to set as pos
-	var pos = $SpawnPositions.get_child(randi()%$SpawnPositions.get_child_count()).global_position
 	for i in range (5):
+		var pos = $SpawnPositions.get_child(randi()%$SpawnPositions.get_child_count()).global_position
 		open.emit(pos, current_direction)
