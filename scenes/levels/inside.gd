2a1ccf5ef0e6886eed6c139c1e2d35a030ca2da6
Legacynical <legacynical@gmail.com>
2024-07-10 00:24:44 -0700

    add item scene

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -1,6 +1,6 @@
 extends LevelParent
 
-@export var outside_level_scene: PackedScene
+#@export var outside_level_scene: PackedScene
 
 func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
