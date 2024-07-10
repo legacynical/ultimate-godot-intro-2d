6279a0f160cfe8f264eebf9311e9734f2a6bb070
Legacynical <legacynical@gmail.com>
2024-07-10 00:03:46 -0700

    add fade to black level transitions

--- scenes/levels/inside.gd
+++ scenes/levels/inside.gd
@@ -5,8 +5,9 @@ extends LevelParent
 func _on_exit_gate_area_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
+	TransitionLayer.change_scene("res://scenes/levels/outside.tscn")
 	#get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
-	call_deferred("change_scene")	
+	#call_deferred("change_scene")	
 	
-func change_scene():
-	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
+#func change_scene():
+	#get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -3,7 +3,8 @@ extends LevelParent
 func _on_gate_player_entered_gate(_body):
 	var tween = create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
-	call_deferred("change_scene")	
+	TransitionLayer.change_scene("res://scenes/levels/inside.tscn")
+	#call_deferred("change_scene")	
 	
-func change_scene():
-	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+#func change_scene():
+	#get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
