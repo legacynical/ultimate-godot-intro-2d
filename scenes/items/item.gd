9dcd57754b72a8ba9bf3d4b8cde94459c8487a6b
Legacynical <legacynical@gmail.com>
2024-07-10 16:40:35 -0700

    add item scale tween

--- scenes/items/item.gd
+++ scenes/items/item.gd
@@ -19,8 +19,10 @@ func _ready():
 
 	# tween
 	var target_pos = position + direction * distance
-	var movement_tween = create_tween()
-	movement_tween.tween_property(self, "position", target_pos, 0.5)
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(self, "position", target_pos, 0.5)
+	tween.tween_property(self, "scale", Vector2(1, 1), 0.3).from(Vector2(0, 0))
 	
 func _process(delta):
 	rotation += rotation_speed * delta
