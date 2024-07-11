ab77bbc9691d17bf8047219391d62c198182083f
Legacynical <legacynical@gmail.com>
2024-07-10 19:52:25 -0700

    add enemy scout alternating laser fire

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 var player_nearby: bool = false
 var can_laser: bool = true
+var laser_alternator: int = 1
+var pos: Vector2
 
 signal laser(pos, direction)
 
@@ -9,7 +11,13 @@ func _process(_delta):
 	if player_nearby:
 		look_at(Globals.player_pos)
 		if can_laser:
-			var pos: Vector2 = $LaserSpawnPositions/Marker2D.global_position
+			var laser_markers = $LaserSpawnPositions.get_children()
+			if laser_alternator == 1:
+				pos = laser_markers[0].global_position
+				laser_alternator = 0
+			else:
+				pos = laser_markers[1].global_position
+				laser_alternator = 1
 			var direction: Vector2 = (Globals.player_pos - position).normalized()
 			laser.emit(pos, direction)
 			can_laser = false
