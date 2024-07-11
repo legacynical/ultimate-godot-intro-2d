1769dfc5a51fb7ec317f7e49bca701f515d6fc8a
Legacynical <legacynical@gmail.com>
2024-07-10 19:36:39 -0700

    refactor level laser code for both player and enemy scout

--- scenes/enemies/scout.gd
+++ scenes/enemies/scout.gd
@@ -1,4 +1,25 @@
 extends CharacterBody2D
 
-func _process(delta):
-	look_at(Globals.player_pos)
+var player_nearby: bool = false
+var can_laser: bool = true
+
+signal laser(pos, direction)
+
+func _process(_delta):
+	if player_nearby:
+		look_at(Globals.player_pos)
+		if can_laser:
+			var pos: Vector2 = $LaserSpawnPositions/Marker2D.global_position
+			var direction: Vector2 = (Globals.player_pos - position).normalized()
+			laser.emit(pos, direction)
+			can_laser = false
+			$LaserCooldown.start()
+			
+func _on_attack_area_body_entered(body):
+	player_nearby = true
+	
+func _on_attack_area_body_exited(body):
+	player_nearby = false
+
+func _on_laser_cooldown_timeout():
+	can_laser = true
--- scenes/levels/signals_level.gd
+++ scenes/levels/signals_level.gd
@@ -8,6 +8,9 @@ var item_scene: PackedScene = preload("res://scenes/items/item.tscn")
 func _ready():
 	for container in get_tree().get_nodes_in_group("Container"):
 		container.connect("open", _on_container_opened)
+	for scout in get_tree().get_nodes_in_group("Scouts"):
+		scout.connect("laser", _on_scout_laser)
+
 
 func _on_container_opened(pos, direction):
 	#print("container opened")
@@ -18,20 +21,22 @@ func _on_container_opened(pos, direction):
 	$Items.call_deferred("add_child", item)
 
 func _on_player_laser(pos, direction):
+	create_laser(pos, direction)
+
+func _on_scout_laser(pos, direction):
+	create_laser(pos, direction)
+	print("scout is shooting")
+	
+func create_laser(pos, direction):
 	var laser = laser_scene.instantiate() as Area2D
+		# creates an Area2D laser scene instance
 	laser.position = pos
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 		# angle method converts vector to radians 
-		# alt: laser.rotation = direction.angle()
+		# alt: laser.rotation = direction.angle()	
 	laser.direction = direction
-	
-	# 1. update the laser position
-	# 2, move the laser
-	# 3. add laser instance to a Node2D
 	$Projectiles.add_child(laser)
-	#$UI.update_laser_text()
-	#print("laser from level")
-
+		# adds the laser instance to parent node Projectiles
 
 func _on_player_grenade(pos, direction):
 	var grenade = grenade_scene.instantiate() as RigidBody2D
@@ -42,7 +47,7 @@ func _on_player_grenade(pos, direction):
 	#print(direction)
 	#print(grenade.speed)
 
-
+	
 # player signal to update stats calls available UI func
 #func _on_globals_stat_change():
 	#$UI.update_stat_text()
