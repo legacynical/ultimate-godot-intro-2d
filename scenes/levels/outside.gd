6c15d88597c46c5bc1a81c089efd5ca63f4ae5e9
Legacynical <legacynical@gmail.com>
2024-07-10 17:01:55 -0700

    remove tween parallel

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -11,8 +11,6 @@ func _on_gate_player_entered_gate(_body):
 
 func _on_house_player_entered():
 	var tween = get_tree().create_tween()
-	tween.set_parallel(true)
-	#tween.tween_property($Player, "modulate:a", 0, 2).from(0.5)
 	tween.tween_property($Player/Camera2D, "zoom", Vector2(1, 1), 1).set_trans(Tween.TRANS_QUAD)
 
 func _on_house_player_exited():
