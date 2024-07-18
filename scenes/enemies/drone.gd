97804cebcf1bcca2413c4c1ea4dccafcdb7b5194
Legacynical <legacynical@gmail.com>
2024-07-18 00:13:52 -0700

    add drone hit timer

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 #var is_enemy: bool = true
 var player_nearby: bool = false
-var speed = 300
+var can_take_dmg: bool = false
+var speed: int = 300
+var health: int = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,8 +20,14 @@ func _process(_delta):
 		move_and_slide()
 
 func hit():
-	print("drone hit")
+	if can_take_dmg:
+		health -= 10
+		can_take_dmg = false
+		$Timers/TakeDmgCooldown.start()
 
-
-func _on_notice_area_body_entered(body):
+func _on_notice_area_body_entered(_body):
 	player_nearby = true
+
+
+func _on_take_dmg_cooldown_timeout():
+	can_take_dmg = true
