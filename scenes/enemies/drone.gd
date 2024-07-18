843358bd149354a246f0a2cf5c418c6e7fc9f955
Legacynical <legacynical@gmail.com>
2024-07-18 01:00:32 -0700

    add drone explosion hit and set collision layers

--- scenes/enemies/drone.gd
+++ scenes/enemies/drone.gd
@@ -9,6 +9,8 @@ var speed: int = 400
 var health: int = 50
 var speed_multiplier: int = 1
 
+var exploding: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Explosion.hide()
@@ -24,6 +26,13 @@ func _process(delta):
 			# unlike move_and_slide(), move_and_collide doesn't include * delta so you need to use it here
 		if collision:
 			$AnimationPlayer.play("explosion")
+			exploding = true
+	if exploding:
+		var targets = get_tree().get_nodes_in_group("Container") + get_tree().get_nodes_in_group("Entity")
+		for target in targets:
+			var in_range = target.global_position.distance_to(global_position) < 400
+			if in_range and "hit" in target:
+				target.hit()
 	
 func hit():
 	if can_take_dmg:
@@ -34,6 +43,7 @@ func hit():
 		$DroneImage.material.set_shader_parameter("progress", 1)
 	if health <= 0:
 		$AnimationPlayer.play("explosion")
+		exploding = true
 
 func stop_movement():
 	speed_multiplier = 0
