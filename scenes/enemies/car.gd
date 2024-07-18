0b3887069d0e03456ebf5b3be7d57dfd724f24f3
Legacynical <legacynical@gmail.com>
2024-07-18 02:11:49 -0700

    add additional car nodes

--- scenes/enemies/car.gd
+++ scenes/enemies/car.gd
@@ -2,3 +2,11 @@ extends PathFollow2D
 
 func _process(delta):
 	progress_ratio += 0.05 * delta
+
+
+func _on_notice_area_body_entered(body):
+	pass # Replace with function body.
+
+
+func _on_notice_area_body_exited(body):
+	pass # Replace with function body.
