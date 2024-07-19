96301c79a3a60ff9deaf02fe6360a4f0b02f6a28
Legacynical <legacynical@gmail.com>
2024-07-19 06:13:34 -0700

    add hunter navigation updater

--- scenes/enemies/hunter.gd
+++ scenes/enemies/hunter.gd
@@ -11,12 +11,11 @@ func _ready():
 	$NavigationAgent2D.target_position = Globals.player_pos
 
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var next_path_pos: Vector2 = $NavigationAgent2D.get_next_path_position()
-	print($NavigationAgent2D.get_current_navigation_path())
 	var direction: Vector2 = (next_path_pos - global_position).normalized()
 	velocity = direction * speed
-	#move_and_slide()k
+	move_and_slide()
 	
 
 func _on_notice_area_body_entered(_body):
@@ -30,3 +29,8 @@ func _on_attack_area_body_entered(_body):
 
 func _on_attack_area_body_exited(_body):
 	player_in_range = false
+
+
+func _on_navigation_timer_timeout():
+	if player_nearby:
+		$NavigationAgent2D.target_position = Globals.player_pos
