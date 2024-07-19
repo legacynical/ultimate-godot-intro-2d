ea1f347fc79db1a77116c8bec2473889c43f9e45
Legacynical <legacynical@gmail.com>
2024-07-19 04:07:25 -0700

    add car turret gun fire

--- scenes/enemies/car.gd
+++ scenes/enemies/car.gd
@@ -5,6 +5,19 @@ var player_nearby: bool = false
 @onready var line1: Line2D = $Turret/RayCast2D/Line2D
 @onready var line2: Line2D = $Turret/RayCast2D2/Line2D
 
+@onready var gun_fire1: Sprite2D = $Turret/GunFire1
+@onready var gun_fire2: Sprite2D = $Turret/GunFire2
+
+func fire():
+	Globals.health -= 20
+	gun_fire1.modulate.a = 1
+	gun_fire2.modulate.a = 1
+	
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property(gun_fire1, "modulate:a", 0, randf_range(0.1, 0.5))
+	tween.tween_property(gun_fire2, "modulate:a", 0, randf_range(0.1, 0.5))
+
 func _ready():
 	line2.add_point($Turret/RayCast2D2.target_position)
 
@@ -16,6 +29,7 @@ func _process(delta):
 
 func _on_notice_area_body_entered(_body):
 	player_nearby = true
+	$AnimationPlayer.play("laser_load")
 
 func _on_notice_area_body_exited(_body):
 	player_nearby = false
