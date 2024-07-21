2515a0e06bfc986ee17a74dc4846a961a5e13c37
Legacynical <legacynical@gmail.com>
2024-07-21 06:47:36 -0700

    add music and sounds

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -5,6 +5,8 @@ var direction: Vector2 = Vector2.UP
 
 func _ready():
 	$LaserExpire.start()
+#	$AudioStreamPlayer2D.play() 
+		# optional way of playing laser sound on creation
 
 func _process(delta):
 	position += direction * speed * delta
